14dd136e255b7ccdcfd8d37c479c15d900a2f7af
shijie.ma <shijie.ma@68aeac6e-0fca-11de-a6db-6dd17253a512>
2010-02-10 16:28:55 +0000

    add query codegenerator UI

---
+++ core/NIntegrate.CodeGenerator/QueryCodeGenerator.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NIntegrate.Data;
+using System.Configuration;
+using NIntegrate.Web;
+using NIntegrate.Data.SqlClient;
+using System.Data;
+
+namespace NIntegrate.CodeGenerator
+{
+    public partial class QueryCodeGenerator : System.Web.UI.Page
+    {
+        private DataView Query(QueryCriteria criteria)
+        {
+            var queryService = new QueryService(new CodeGeneratorQueryCommandFactory(tbConnStr.Text));
+            return new DataView(queryService.Query(criteria));
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tbOutputDir.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        }
+
+        protected void btnConnect_Click(object sender, EventArgs e)
+        {
+            var sysObjs = new sysobjects();
+            cblTables.DataSource = Query(sysObjs.Select(sysObjs.name).And(sysObjs.xtype == "U" && sysObjs.name != "sysdiagrams").SortBy(sysObjs.name, false));
+            cblViews.DataSource = Query(sysObjs.Select(sysObjs.name).And(sysObjs.xtype == "V").SortBy(sysObjs.name, false));
+            cblSprocs.DataSource = Query(sysObjs.Select(sysObjs.name).And(sysObjs.xtype == "P" && !sysObjs.name.StartsWith("dt_") && !sysObjs.name.Like("sp_%diagram%")).SortBy(sysObjs.name, false));
+
+            DataBind();
+        }
+    }
+
+    #region Query Classes
+
+    public class CodeGeneratorQueryCommandFactory : QueryCommandFactory
+    {
+        private readonly ConnectionStringSettings _connStr;
+
+        public CodeGeneratorQueryCommandFactory(string connStr)
+        {
+            _connStr = new ConnectionStringSettings("Default", connStr);
+        }
+
+        protected override System.Configuration.ConnectionStringSettings GetConnectionString(string connectionStringName)
+        {
+            return _connStr;
+        }
+    }
+
+    public class sysobjects : QueryTable
+    {
+        public sysobjects()
+            : base("sysobjects", "Default", true)
+        {
+        }
+
+        public StringColumn name = new StringColumn("name", true);
+        public StringColumn xtype = new StringColumn("xtype", false);
+    }
+
+    #endregion
+}
--- core/NIntegrate.Web/QueryService.cs
+++ core/NIntegrate.Web/QueryService.cs
@@ -10,18 +10,32 @@ namespace NIntegrate.Web
     /// </summary>
     public sealed class QueryService : IQueryService
     {
+        private readonly QueryCommandFactory _cmdFac;
+
+        public QueryService()
+        {
+            _cmdFac = new QueryCommandFactory();
+        }
+
+        public QueryService(QueryCommandFactory cmdFac)
+        {
+            if (cmdFac == null)
+                throw new ArgumentNullException("cmdFac");
+
+            _cmdFac = cmdFac;
+        }
+
         #region IQueryService Members
 
         public DataTable Query(QueryCriteria criteria)
         {
-            var fac = new QueryCommandFactory();
-            var cmd = fac.CreateCommand(criteria, false);
+            var cmd = _cmdFac.CreateCommand(criteria, false);
             using (var conn = cmd.Connection)
             {
                 if (conn.State != ConnectionState.Open)
                     conn.Open();
                 var result = new DataTable(criteria.TableName) {Locale = CultureInfo.InvariantCulture};
-                var adapter = fac.GetDbProviderFactory(criteria).CreateDataAdapter();
+                var adapter = _cmdFac.GetDbProviderFactory(criteria).CreateDataAdapter();
                 adapter.SelectCommand = cmd;
                 adapter.Fill(result);
                 return result;
@@ -30,8 +44,7 @@ namespace NIntegrate.Web
 
         public int Execute(QueryCriteria criteria, bool isCountQuery)
         {
-            var fac = new QueryCommandFactory();
-            var cmd = fac.CreateCommand(criteria, isCountQuery);
+            var cmd = _cmdFac.CreateCommand(criteria, isCountQuery);
             using (var conn = cmd.Connection)
             {
                 if (conn.State != ConnectionState.Open)
